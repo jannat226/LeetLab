b7fb7eee9a0f13ef7819d1fffeb62d9b03c3482e
Jannat226 <jannat@mca.christuniversity.in>
2025-06-16 03:33:54 -0700

    auth controllers, routes, middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 const app = express();
-app.listen(process.env.PORT, () => {
-  console.log("hello world");
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  console.log("hello all , welcome to the leetlab!!");
+  res.send("welcome there!");
 });
-app.use(express.json);
+
+app.use("/api/v1/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
